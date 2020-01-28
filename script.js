47a16f7694dccc155c30b389e69ea82bdfac5393
David Joseph <56838866+davidcbalsley@users.noreply.github.com>
2020-01-28 03:44:18 -0600

    render one row

--- script.js
+++ script.js
@@ -1,39 +1,55 @@
-// Create rows for the hours of the day and display them
-function renderHourlyTimeBlocks () {
+// Time slots
+var timeSlots = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
+
+// Create a time slot and display it
+function renderOneHourlyTimeBlock () {
     // As a first step, create one new row and display it
-    // var newRow = $("<row>");
     var newRow = $("<div>");
     newRow.attr("class", "row")
-    // newRow.text("Here is a row!"); // debug
+    
+    // Create a blank buffer column for the left
+    var newLeftBufferCol = $("<div>");
+    newLeftBufferCol.attr("class", "col-1");
 
     // Create a column for the time
     var newTimeCol = $("<div>");
-    newTimeCol.attr("class", "col-md-2");
-    // newTimeCol.text("Here is the time!"); // debug
-    var newTimePar = $("<p>");
-    newTimePar.text("Here is the time!");
-    newTimeCol.append(newTimePar);
-
+    newTimeCol.attr("class", "col-1 hour");
+    
     // Create a column for the appointment description
     var newAppointmentCol = $("<div>");
-    newAppointmentCol.attr("class", "col-md-8");
-    // newAppointmentCol.text("Here is the appointment!"); // debug
-    var newAppointmentPar = $("<p>");
-    newAppointmentPar.text("Here is the appointment!");
-    newAppointmentCol.append(newAppointmentPar);
+    newAppointmentCol.attr("class", "col-8 description");
     
     // Create a column for the save button
     var newSaveCol = $("<div>");
-    newSaveCol.attr("class", "col-md-2");
-    newSaveCol.text("Save column!");
+    newSaveCol.attr("class", "col-1 saveBtn d-flex");
 
-    newRow.append(newTimeCol, newAppointmentCol, newSaveCol);
+    // Create a new save button
+    var newSaveBtn = $("<button>");
+    newSaveBtn.attr("class", "btn btn-block");
+
+    // Add the lock icon to the button
+    var newButtonLabel = $("<span>");
+    newButtonLabel.attr("class", "fa fa-lock");
+    newButtonLabel.attr("style", "color:#d3d3d3");
+    newSaveBtn.append(newButtonLabel);
+
+    newSaveCol.append(newSaveBtn);
+
+    // Create a blank buffer column for the right
+    var newRightBufferCol = $("<div>");
+    newRightBufferCol.attr("class", "col-1");
+
+    // Append the time, description, and save button to the row
+    newRow.append(newLeftBufferCol, newTimeCol, newAppointmentCol, newSaveCol);
     
+    // Append the row 
+    // $("#hourly-time-blocks").append(newRow);
     $("#hourly-time-blocks").append(newRow);
-
 }
+
+// Create all of the time slots and display them
 
 // On page load --
 // - Create the hourly time blocks and display them
 // - Load any existing appointments and display them in the time blocks
-renderHourlyTimeBlocks();
+renderOneHourlyTimeBlock();
